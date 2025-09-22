d0049165e85ed5e96f368494be111a86bc73c7f9
JulienMaido <j.delgard@hotmail.fr>
2025-09-22 23:48:07 +0200

    chore:(gitlab): configuration de gitlab pages

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-const basename = import.meta.env.DEV ? "/" : "/sefosa85";
+const basename = import.meta.env.VITE_BASE_PATH || "/";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/sefosa85/" : "/", // 👈 Prod = /sefosa85/, Dev = /
+  base: command === "build" ? process.env.VITE_BASE_PATH || "/" : "/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
